9a29cdec4820c6965a2ef546c19aafccec1c01e8
mrmaxxgen <kevindetogni03@gmail.com>
2026-06-22 16:11:42 +0200

    coded main code, needs fix, implemented mp3 and mp4 support

--- README.md
+++ README.md
@@ -5,8 +5,11 @@ mrmaxxgen (c) 2026, licensed under the GNU General Public License v3 <https://ww
 
 mindexer is a file indexer/sorter utility for automatic by-type file sorting/indexing in specific folder, it can be used for servers html's pages or just for tidying up your system with zero effort, mindexer handles (almost) everything by itself thru commands and error handling, it will also help you creating a base to start using it.
 
-current version: 0.0.0 (development starting soon)
+current version: 0.0.1 ALPHA
 
 changelog:
 
--created repo and code base
+0.0.1 ALPHA:
+    -created repo and code base
+    -coded main code, not working, needs fix
+    -implemented mp3 and mp4 support for now
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@
 use std::io;
 use std::process::Command;
 
-fn main() {
-    
+fn main() {    
     /*mindexer's purpose is very simple: to sort all files in one big folder called "sortfolder" into multiple smaller folder
      *for file type like "videos", "songs", "repos" ecc... it works by spawning shells commands that moves each file into a
      *specific folder by it's type, like "song.mp3" from "sortfolder" directory to "songs" directory.
@@ -15,10 +14,52 @@ fn main() {
      *for compilation and usage instructions, see README.MD or mindexer github page <https://github.com/mrmaxxgen/mindexer.git>
     */
 
-    /*steps: -check if mindexerbase exists. if not, ask user to create it.
-     *       -check if command is running inside mindexerbase. if not, exit.
+    /*steps: -check if mindexerbase exists.
+     *       -cd into mindexerbase.
      *       -move videos from "sortfolder" dir to "videos" dir.
      *       -move songs from "sortfolder" dir to "songs" dir.  
     */
-    
+
+    match Command::new("ls")
+        .arg("mindexerbase mindexerbase/sortfolder mindexerbase/videos mindexerbase/songs")
+        .output() {            
+            Ok(_) => {
+                match Command::new("cd")
+                    .arg("mindexerbase")
+                    .output() {
+                        Ok(_) => {
+                            match Command::new("mv")
+                                .arg("mindexerbase/sortfolder/*.mp4 mindexerbase/sortfolder/*.mp3")
+                                .output() {
+                                    Ok(_) => {
+                                        println!("moved files to respective folders succesfully");
+                                    }
+                                    Err(_) => {
+                                        println!("unable to sort files, check permission and/or relaunch");
+                                    }
+                                }
+                        }
+                        Err(_) => {
+                            println!("unable to cd into mindexerbase, check permission and/or relaunch, or cd manually");
+                        }
+                    }
+            }
+            Err(_) => {   
+                println!("[?] mindexerbase not found. create it? (y/n):");
+                let mut base_maker_ask = String::new();
+                io::stdin().read_line(&mut base_maker_ask).expect("failed to execute command");
+                if base_maker_ask == "y" {
+                    match Command::new("mkdir")
+                        .arg("mindexerbase mindexerbase/sortfolder mindexerbase/videos mindexerbase/songs")
+                        .output() {
+                            Ok(_) => {
+                                println!("[i] mindexerbase created succesfully, relaunch mindexer in the directory \"mindexerbase\".");
+                            }
+                            Err(_) => {
+                                println!("[!] failed to create mindexerbase, check permission and/or relaunch.");
+                            }
+                        }
+                }
+            }
+        }
 }
