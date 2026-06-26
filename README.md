d869a014ecb2c52367798b4955b6ae91d5b93e77
mrmaxxgen <kevindetogni03@gmail.com>
2026-06-26 19:14:33 +0200

    version 0.0.2 ALPHA incomplete

--- README.md
+++ README.md
@@ -3,13 +3,23 @@ a simple file indexer written in Rust
 
 mrmaxxgen (c) 2026, licensed under the GNU General Public License v3 <https://www.gnu.org/licenses/>
 
-mindexer is a file indexer/sorter utility for automatic by-type file sorting/indexing in specific folder, it can be used for servers html's pages or just for tidying up your system with zero effort, mindexer handles (almost) everything by itself thru commands and error handling, it will also help you creating a base to start using it.
+mindexer is a file indexer/sorter utility for automatic by-type file sorting/indexing in specific folder, it can be used for servers html's pages or just for tidying up your system with zero effort, mindexer handles (almost) everything by itself thru fs/env calls and error handling, it will also help you creating a base to start using it.
 
-current version: 0.0.1 ALPHA
+current version: 0.0.2 ALPHA (pre-release)
+
+for future releases:
+    -configure Makefile
+    -fix issues (marked in main.rs)
+    -check licensing
 
 changelog:
 
-0.0.1 ALPHA:
+0.0.1 ALPHA (pre-release):
     -created repo and code base
     -coded main code, not working, needs fix
     -implemented mp3 and mp4 support for now
+
+0.0.2 ALPHA (pre-release):
+    -ditched commands and used rust fs/env libraries instead
+    -commented code more
+    -made code lighter
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,78 @@
-/*mindexer (c) mrmaxxgen 2026
- *licensed under GNU GPLv3 or later <https://www.gnu.org/licenses/>
- */
+// mindexer (c) mrmaxxgen 2026
+// licensed under GNU GPLv3 or later <https://www.gnu.org/licenses/>
+
 use std::io;
-use std::process::Command;
+use std::fs;
+use std::env;
+use glob::glob;
+use std::path::Path;
 
 fn main() {    
-    /*mindexer's purpose is very simple: to sort all files in one big folder called "sortfolder" into multiple smaller folder
-     *for file type like "videos", "songs", "repos" ecc... it works by spawning shells commands that moves each file into a
-     *specific folder by it's type, like "song.mp3" from "sortfolder" directory to "songs" directory.
-     *if mindexer doesn't detects the base, it will create it, or, if it's not running inside the base, it will simply
-     *outputs an error message and quit. throughout the base making and file sorting, each file and base modification will
-     *be logged and reported on the terminal like "[ok] sorting file <FILE> into folder <FOLDER>". that's all! :3
-     *for compilation and usage instructions, see README.MD or mindexer github page <https://github.com/mrmaxxgen/mindexer.git>
-    */
+    // mindexer's purpose is very simple: to sort all files in one big folder called "sortfolder" into multiple smaller folder
+    // for file type like "videos", "songs", "repos" ecc... it works by spawning shells commands that moves each file into a
+    // specific folder by it's type, like "song.mp3" from "sortfolder" directory to "songs" directory.
+    // if mindexer doesn't detects the base, it will create it, or, if it's not running inside the base, it will simply
+    // outputs an error message and quit. throughout the base making and file sorting, each file and base modification will
+    // be logged and reported on the terminal like "[ok] sorting file <FILE> into folder <FOLDER>". that's all! :3
+    // for compilation and usage instructions, see README.MD or mindexer github page <https://github.com/mrmaxxgen/mindexer.git>
 
-    /*steps: -check if mindexerbase exists.
-     *       -cd into mindexerbase.
-     *       -move videos from "sortfolder" dir to "videos" dir.
-     *       -move songs from "sortfolder" dir to "songs" dir.  
-    */
+    // steps: 
+    //        - check if mindexerbase exists.
+    //        - cd into mindexerbase.
+    //        ! move videos from "sortfolder" dir to "videos" dir.
+    //        ! move songs from "sortfolder" dir to "songs" dir.
 
-    match Command::new("ls")
-        .arg("mindexerbase mindexerbase/sortfolder mindexerbase/videos mindexerbase/songs")
-        .output() {            
-            Ok(_) => {
-                match Command::new("cd")
-                    .arg("mindexerbase")
-                    .output() {
-                        Ok(_) => {
-                            match Command::new("mv")
-                                .arg("mindexerbase/sortfolder/*.mp4 mindexerbase/sortfolder/*.mp3")
-                                .output() {
-                                    Ok(_) => {
-                                        println!("moved files to respective folders succesfully");
-                                    }
-                                    Err(_) => {
-                                        println!("unable to sort files, check permission and/or relaunch");
-                                    }
-                                }
-                        }
-                        Err(_) => {
-                            println!("unable to cd into mindexerbase, check permission and/or relaunch, or cd manually");
-                        }
+    // structure: 
+    //            - core function (files indexing)
+    //            - base creator
+
+    // issues: 
+    //         TODO: allow wildcards with glob
+    //         TODO: solve loop-printing in base creator section
+
+    if Path::new("mindexerbase").is_dir() { // check if mindexerbase directory exists
+        match env::set_current_dir("mindexerbase") { // cd into mindexerbase directory
+            Ok(()) => {
+                match fs::copy("sortfolder/*.mp4", "videos/*.mp4") { // ! moves videos to videos directory
+                    Ok(_) => {
+                        println!("[i] videos moved succesfully.") // prints info if it moved videos succesfully
                     }
-            }
-            Err(_) => {   
-                println!("[?] mindexerbase not found. create it? (y/n):");
-                let mut base_maker_ask = String::new();
-                io::stdin().read_line(&mut base_maker_ask).expect("failed to execute command");
-                if base_maker_ask == "y" {
-                    match Command::new("mkdir")
-                        .arg("mindexerbase mindexerbase/sortfolder mindexerbase/videos mindexerbase/songs")
-                        .output() {
-                            Ok(_) => {
-                                println!("[i] mindexerbase created succesfully, relaunch mindexer in the directory \"mindexerbase\".");
-                            }
-                            Err(_) => {
-                                println!("[!] failed to create mindexerbase, check permission and/or relaunch.");
-                            }
-                        }
+                    Err(e) => {
+                        println!("[!] unable to move videos, {}", e) // prints error if it didn't moved videos
+                    }
                 }
+                match fs::copy("sortfolder/*.mp3", "songs/*.mp3") { // ! moves songs to songs directory
+                    Ok(_) => {
+                        println!("[i] songs moved succesfully.") // prints info if it moved songs succesfully
+                    }
+                    Err(e) => {
+                        println!("[!] unable to move songs, {}", e) // prints error if it didn't moved songs
+                    }
+                }
+            }  
+            Err(e) => {
+                println!("[!] unable to cd into mindexerbase, {}", e) // prints error if it didn't cd into mindexerbase
             }
         }
+
+    } else {
+        println!("[?] mindexerbase not found, create it? (y/n):"); // asks user if he wants to create mindexerbase
+        let mut base_maker_ask = String::new();
+        io::stdin().read_line(&mut base_maker_ask).expect("failed to read line");
+        if base_maker_ask.trim() == "y" {
+            let basedirs = ["mindexerbase/sortfolder", "mindexerbase/videos", "mindexerbase/songs"];
+            for dir in basedirs {
+                match fs::create_dir_all(dir) { // creates mindexerbase if user agreed
+                    Ok(_) => {
+                        println!("[i] mindexerbase created succesfully, rerun.") // prints info if it created mindexerbase succesfully
+                    }
+                    Err(e) => {
+                        println!("[!] unable to create mindexerbase, {}", e) // prints error if it didn't created mindexerbase
+                    }
+                }
+            }
+        } else {
+            println!("[i] aborting") // do nothing if user declined
+        }
+    }
 }
